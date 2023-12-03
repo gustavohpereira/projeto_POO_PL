9c0a8098a41803f548673c8278152ab91c460198
gustavohpereira <gustavohpa@bricker.vc>
2023-12-03 10:19:25 -0300

    Atualizar Clientes

--- atividade_4/front-js/src/componentes/listaCliente.js
+++ atividade_4/front-js/src/componentes/listaCliente.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+
 export default function ListaCliente(props) {
     
     const [clients, setClients] = useState([])
@@ -22,6 +23,7 @@ export default function ListaCliente(props) {
     }, [])
 
     const [clienteSelecionado, setClienteSelecionado] = useState(null)
+    const [AtualizarCliente, setAtualizarCliente] = useState(null)
 
     const handleVisualizar = (client) => {
         setClienteSelecionado(client);
@@ -29,22 +31,43 @@ export default function ListaCliente(props) {
     
     const handleVoltar = () => {
         setClienteSelecionado(null);
+        setAtualizarCliente(null)
     };
 
+    function onAtualizar(client) {
+        setAtualizarCliente(client);
+    }
+
+
+
     const handleDeletar = async (client) => {
-        console.log(client)
+
         await axios.delete("http://localhost:32831/cliente/excluir", {data:{id:client.id}}).then(() => {
             window.location.reload()    
         })
     }
 
+    if (clienteSelecionado) {
+        return (
+            <div>
+                <VisualizarCliente client={clienteSelecionado} handleVoltar={handleVoltar} />
+            </div>
+            
+        )
+    }
+    if (AtualizarCliente) {
+
+        return (
+
+                <AtualizarClienteFunction client={AtualizarCliente} handleVoltar={handleVoltar} />
+        )
+    }
+
     return (
         <div>
-            {clienteSelecionado ? (
-                <VisualizarCliente client={clienteSelecionado} handleVoltar={handleVoltar} />
-            ): (
-                <TabelaClientes onVisualizar={handleVisualizar} handleDeletar={handleDeletar} clients={clients} />
-            )}
+           
+                <TabelaClientes onVisualizar={handleVisualizar} onAtualizar={onAtualizar} handleDeletar={handleDeletar} clients={clients} />
+            
         </div>
     );
 }
@@ -65,9 +88,10 @@ export function TabelaClientes(props) {
                         <tr key={index} style={{ backgroundColor: props.tema }}>
                             <td>{client.nome}</td>
                             <td>{client.nomeSocial}</td>
-                            <td>
-                                <button onClick={() => props.onVisualizar(client)}>Visualizar</button>
-                                <button onClick={() => props.handleDeletar(client)}>Excluir</button>
+                            <td className="btn-group">
+                                <button className="btn btn-outline-secondary" onClick={() => props.onVisualizar(client)}>Visualizar</button>
+                                <button className="btn btn-outline-secondary" onClick={() => props.handleDeletar(client)}>Excluir</button>
+                                <button className="btn btn-outline-secondary" onClick={() => props.onAtualizar(client)}>Atualizar</button>
                             </td>
                         </tr>
                     ))}
@@ -133,3 +157,102 @@ export function VisualizarCliente(props) {
         </div>
     );
 }
+
+export function AtualizarClienteFunction (props){
+
+
+  
+    const [clienteData, setClienteData] = useState(props.client);
+
+    let tema = "#e3f2fd";
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        const updatedClienteData = { ...clienteData };
+
+        if (name.startsWith('endereco')) {
+            const enderecoField = name.split('.')[1];
+            updatedClienteData.endereco = {
+                ...updatedClienteData.endereco,
+                [enderecoField]: value,
+            };
+        }
+        else if (name.startsWith('telefones')) {
+            const telefoneField = name.split('.')[1];
+            updatedClienteData.telefones[0] = {
+                ...updatedClienteData.telefones[0],
+                [telefoneField]: value,
+            };
+        }
+        else {
+            updatedClienteData[name] = value;
+        }
+
+        setClienteData(updatedClienteData);
+    };
+
+    const handleAtualizar = async () => {
+        try {
+          await axios.put("http://localhost:32831/cliente/atualizar", clienteData);
+          // Do something after a successful update, e.g., redirect or reload data
+          window.location.reload();
+        } catch (error) {
+          console.error(error);
+          // Handle error, show a message to the user, etc.
+        }
+      };
+
+
+        return(
+        <div className="container-fluid">
+            <h1 className="font-3 mb-3">Atualizar cliente</h1>
+            <form>
+                <div className="input-group mb-3">
+                    <input type="text" name="nome" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.nome} />
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="nomeSocial" className="form-control" placeholder="Nome social" aria-label="Nome social" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.nomeSocial} />
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1" style={{ background: tema }}>@</span>
+                    <input type="text" name="email" className="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="basic-addon1" onChange={handleInputChange} value = {clienteData.email} />
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="endereco.estado" className="form-control" placeholder="Estado" aria-label="Estado" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.endereco.estado} />
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="endereco.cidade" className="form-control" placeholder="Cidade" aria-label="Cidade" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.endereco.cidade}/>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="endereco.bairro" className="form-control" placeholder="Bairro" aria-label="Bairro" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.endereco.bairro} />
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="endereco.rua" className="form-control" placeholder="Rua" aria-label="Rua" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.endereco.rua}/>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="endereco.numero" className="form-control" placeholder="Número" aria-label="Número" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.endereco.numero}/>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="endereco.codigoPostal" className="form-control" placeholder="Código postal" aria-label="Código postal" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.endereco.codigoPostal}/>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="endereco.informacoesAdicionais" className="form-control" placeholder="Informações adicionais" aria-label="Informações adicionais" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.endereco.informacoesAdicionais}/>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="telefones[0].ddd" className="form-control" placeholder="DDD" aria-label="DDD" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.telefones[0].ddd} />
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" name="telefones[0].numero" className="form-control" placeholder="Número de telefone" aria-label="Número de telefone" aria-describedby="basic-addon1" onChange={handleInputChange} value={clienteData.telefones[0].numero}/>
+                </div>
+
+                <div className="input-group mb-3">
+                    <button className="btn btn-outline-secondary" type="button" style={{ background: tema }} onClick={handleAtualizar}>Atualizar</button>
+                </div>
+            </form>
+            <button  className="btn btn-outline-secondary" onClick={props.handleVoltar}>Voltar</button>
+    </div>
+    )
+}
+
+
